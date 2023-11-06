8d320e15fd3ab7c487d08a1c07c13499c739e9e6
birajchhetri30 <birajchhetri3824@gmail.com>
2023-11-06 15:22:59 +0545

    Update NavGraph.kt

--- app/src/main/java/com/example/snacksquad/NavGraph.kt
+++ app/src/main/java/com/example/snacksquad/NavGraph.kt
@@ -5,8 +5,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.NavHostController
